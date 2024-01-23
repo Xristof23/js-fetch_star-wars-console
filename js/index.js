a500eddeb6013d26511a0789f8341cd20f422f39
Christof Röhl <userpattern@gmail.com>
2024-01-23 10:22:24 +0100

    solved

--- js/index.js
+++ js/index.js
@@ -2,6 +2,13 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch("https://swapi.dev/api/people");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.results[2]);
+  console.log("rsds2s eye color: " + data.results[2].eye_color);
+}
 
 fetchData();
